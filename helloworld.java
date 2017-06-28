b3d4d56a001a18eb229074e8553dfd99b7eee998
vedhanayaki <vedhanayaki1996@gmail.com>
2017-06-28 15:15:59 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+package guviset3;
+import java.util.Scanner;
+public class helloworld {
+
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	String s1=s.next();
+	System.out.println(s1);
+	}
+
+}
